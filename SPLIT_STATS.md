89f711b492d1e4e8e47b5385dece8fe53557acec
anaselaraby4459-oss <anas.elaraby4459@gmail.com>
2026-05-10 21:39:10 +0300

    Add files via upload

---
+++ SPLIT_STATS.md
@@ -0,0 +1,8 @@
+# Split stats
+
+| Page | Original HTML | New HTML | CSS | JS | Styles | Inline scripts |
+|---|---:|---:|---:|---:|---:|---:|
+| index.html | 3,983 | 2,067 | 1,547 | 571 | 1 | 2 |
+| admin_secure_live_classroom_v15_customization_studio.html | 312,423 | 64,619 | 32,907 | 201,646 | 12 | 13 |
+| student_secure_live_classroom_v15_customization_studio.html | 217,505 | 19,215 | 44,099 | 149,330 | 11 | 14 |
+| parent_portal_v15_customization_studio.html | 13,774 | 2,209 | 2,129 | 9,842 | 3 | 3 |
---
+++ service-worker.js
@@ -0,0 +1,5 @@
+const CACHE_NAME='titan-v16-shell-v1';
+const ASSETS=['./','./index.html','./student_secure_live_classroom_v15_customization_studio.html','./admin_secure_live_classroom_v15_customization_studio.html','./parent_portal_v15_customization_studio.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)).catch(()=>{})); self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))); self.clients.claim();});
+self.addEventListener('fetch',event=>{const req=event.request; if(req.method!=='GET') return; event.respondWith(fetch(req).then(res=>{try{const copy=res.clone(); if(new URL(req.url).origin===location.origin)caches.open(CACHE_NAME).then(c=>c.put(req,copy));}catch(e){} return res;}).catch(()=>caches.match(req).then(r=>r||caches.match('./index.html'))));});
